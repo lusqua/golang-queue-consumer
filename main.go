29056dcde31d7196505b00017de27d3e00287a9b
lusqua <lucasaguiar.floripa@gmail.com>
2023-10-30 13:58:32 -0300

    Feat. change to current ip

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@10.244.0.50:5672/")
 	if err != nil {
 		panic(err)
 	}
